a65014468a5ac5ce4eacd06993379c4fc639feb4
Tanguy Nodet <imrahil.belfalas@gmail.com>
2015-03-21 10:55:34 +0100

    Bordel

--- ProjetInfo/src/CentreGravite.java
+++
@@ -1,9 +0,0 @@
-public class CentreGravite extends Point { //Objet de type point pour décrire le centre de gravité d'un Objet
-
-	public CentreGravite(double ax, double ay) {
-		super(ax, ay);
-	}
-
-	
-	
-}

e54abf76da26fdebdfceb45fda35c76b39b10117
Tanguy Nodet <imrahil.belfalas@gmail.com>
2015-03-21 11:24:28 +0100

    Il faut toujours commencer par un petit "Hello World" !

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-    System.out.println("Test de synchro");
-  }
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		/* Il faut toujours un petit "Hello World" pour bien commencer */
+		System.out.println(":)")
+	}
+	
 }
---
+++ Projet-Info/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		/* Il faut toujours un petit "Hello World" pour bien commencer */
+		System.out.println(":)");
+	}
+	
+}

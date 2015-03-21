bb6e9f4622a9831993f8619d15a2da71bbff1f5d
Tanguy Nodet <imrahil.belfalas@gmail.com>
2015-03-21 10:25:46 +0100

    Début de codes de différentes classes
    Signed-off-by: Tanguy Nodet <imrahil.belfalas@gmail.com>

---
+++ ProjetInfo/src/CentreGravite.java
@@ -0,0 +1,9 @@
+public class CentreGravite extends Point { //Objet de type point pour décrire le centre de gravité d'un Objet
+
+	public CentreGravite(double ax, double ay) {
+		super(ax, ay);
+	}
+
+	
+	
+}

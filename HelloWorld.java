62da73b3db6c57a20d7862fda64cbfc1c72bf441
Tanguy Nodet <imrahil.belfalas@gmail.com>
2015-03-21 11:32:26 +0100

    Ce coup ci, c'est tout propre !

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		/* Il faut toujours un petit "Hello World" pour bien commencer */
-		System.out.println(":)")
-	}
-	
-}

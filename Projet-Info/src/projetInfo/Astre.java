d1c6ae8f17b5ea9cfcc7f5f022ff1bdd46c71783
Ben <Ben@wifinsa118.insa-lyon.fr>
2015-03-30 15:04:24 +0200

    objet astre et astre spherique

---
+++ Projet-Info/src/projetInfo/Astre.java
@@ -0,0 +1,17 @@
+package projetInfo;
+
+import java.awt.Rectangle;
+import java.util.LinkedList;
+
+public abstract class Astre extends Objet{
+	
+	static LinkedList<Astre> liste = new LinkedList<Astre> ();
+	
+public Astre(int ax, int ay, float adx, float ady, float avitesse,
+			String[] NomImage, Rectangle aframe, String nom, int nbIm, int masse) {
+		super(ax, ay, adx, ady, avitesse, NomImage, aframe, nom, nbIm, masse);
+		liste.add(this);
+	}
+
+
+}
---
+++ Projet-Info/src/projetInfo/AstreSpherique.java
@@ -0,0 +1,23 @@
+package projetInfo;
+
+import java.awt.Rectangle;
+
+import javafx.scene.shape.Circle;
+
+public class AstreSpherique extends Astre {
+	Circle limites;
+
+	public AstreSpherique(int ax, int ay, float adx, float ady, float avitesse,
+			String[] NomImage, Rectangle aframe, String nom, int nbIm, int masse, double rayon) {
+		super(ax, ay, adx, ady, avitesse, NomImage, aframe, nom, nbIm, masse);
+		limites = new Circle(rayon);
+	}
+
+	@Override
+	public void move(long t) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}

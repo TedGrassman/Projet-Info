bea4296c7f6a9536e2625fc3c6aab8fb6e39350c
Tanguy Nodet <imrahil.belfalas@gmail.com>
2015-03-30 14:44:30 +0200

    Ajout de la base

--- Projet-Info/src/projetInfo/Objet.java
+++ Projet-Info/src/projetInfo/Objet.java
@@ -24,8 +24,8 @@ public abstract class Objet {
 	String nom_objet; // Nom de l'objet
 	Boolean actif; // Si l'objet est actif ou non
 	int NbImages; // Nombre d'images ou sprites pour l'objet
-	int masse; // Masse de l'objet (pour l'action de la gravité
-	CentreGravite centreG; //centre de gravitŽ de l'objet
+	int masse; // Masse de l'objet (pour l'action de la gravit
+	CentreGravite centreG; //centre de gravité de l'objet
 
 	public Objet(int ax, int ay, float adx, float ady, float avitesse, String[] NomImage,
 			Rectangle aframe, String nom, int nbIm, int masse) {

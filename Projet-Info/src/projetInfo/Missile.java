87b8f27aa6c5108c203c06aa1351220eac26f0d4
FALLOUH <FALLOUH@10.43.6.157>
2015-04-27 14:45:21 +0200

    fin moteur

--- Projet-Info/src/projetInfo/Missile.java
+++ Projet-Info/src/projetInfo/Missile.java
@@ -18,14 +18,24 @@ public class Missile extends Astre {
 		int cAstres =0;
 		double xAstre=0.0;
 		double yAstre=0.0;
+		double teta=0.0;
 		int masse =0;
+		double vitesse=0;
 		while (liste.get(cAstres)!=null){
 			xAstre= liste.get(cAstres).centreG.x;
 			yAstre= liste.get(cAstres).centreG.y;
 			masse = liste.get(cAstres).masse;
 			// determiner angle a partir de deltax et deltay. Calculer force en norme. Projeter en dx dy.
+			teta=Math.atan2(yAstre-this.y, xAstre-this.x);
+			vitesse = (masse*this.masse)/((yAstre-this.y)*(yAstre-this.y)+(xAstre-this.x)*(xAstre-this.x));
+			this.dx=(float) (dx+vitesse*Math.cos(teta));
+			this.dy=(float) (dy+vitesse*Math.sin(teta));
 		}
-		
+		this.x=(int) (x+this.dx);
+		this.y=(int) (y+this.dy);
+		limites.setTranslateX(this.dx);
+		limites.setTranslateY(this.dy);
+		limites.setRotate(teta-limites.getRotate());
 	}
 
 }

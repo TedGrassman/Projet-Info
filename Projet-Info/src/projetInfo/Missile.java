e7fab6f64bcafa4cd87e11832ee915f4f7d2fe37
FALLOUH <FALLOUH@134.214.59.75>
2015-05-04 14:50:13 +0200

    missile2

--- Projet-Info/src/projetInfo/Missile.java
+++ Projet-Info/src/projetInfo/Missile.java
@@ -20,7 +20,7 @@ public class Missile extends Astre {
 	Polygon limites; //hitbox triangulaire
 	double angle; //orientation du missile
 	static final int MASSE_MISSILE=10;
-	static String[] NomImage = {"missile.png"};
+	static String[] NomImage = {"missile2.png"};
 
 	public Missile(int ax, int ay, Rectangle aframe, String[] tab) {
 		super(ax, ay, 0, 0, tab, aframe, "Missile", 1, MASSE_MISSILE);

13bf0cd93dd4252156b5fcde20768ac0277fff4d
FALLOUH <FALLOUH@10.44.2.29>
2015-05-30 02:42:53 +0200

    Ajout d'une musique au menu (changeable...) Simplification du mécanisme de pause Anti aliasing pour les textes

---
+++ Projet-Info/src/framework/Son.java
@@ -0,0 +1,58 @@
+package framework;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class Son {
+	Clip clip;
+	
+	public Son(String nomFichier){ //fonctionne uniquement avec les fichiers .wav !
+		 try {
+	            File file = new File(nomFichier);
+	            if (file.exists()) {
+	                AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+	                clip = AudioSystem.getClip(); //charge le son en mémoire
+	                clip.open(sound);
+	            }
+	            else {
+	                throw new RuntimeException("Fichier son non trouvé :" + nomFichier);
+	            }
+	        }
+	        catch (MalformedURLException e) {
+	            e.printStackTrace();
+	            throw new RuntimeException("URL son invalide !" + e);
+	        }
+	        catch (UnsupportedAudioFileException e) {
+	            e.printStackTrace();
+	            throw new RuntimeException("Fichier son non supporté" + e);
+	        }
+	        catch (IOException e) {
+	            e.printStackTrace();
+	            throw new RuntimeException("Erreur IO" + e);
+	        }
+	        catch (LineUnavailableException e) {
+	            e.printStackTrace();
+	            throw new RuntimeException("Line Unavailable " + e);
+	        }
+	}
+	
+	public void jouer(){			//joue le son
+        clip.setFramePosition(0);  // repart de 0
+        clip.start();
+    }
+	
+    public void boucle(){			//fait jouer le son en boucle
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+    public void stop(){				//arrête la lecture
+            clip.stop();
+        }
+
+}

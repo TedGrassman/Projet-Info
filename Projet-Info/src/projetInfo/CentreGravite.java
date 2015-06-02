d3dc9db9c0337e923f2138a8035a60866c00a6c5
FALLOUH <FALLOUH@10.44.2.29>
2015-06-02 13:27:44 +0200

    suppression du package inutile

--- Projet-Info/src/projetInfo/CentreGravite.java
+++
@@ -1,9 +0,0 @@
-package projetInfo;
-
-public class CentreGravite extends Point { //Objet de type point pour décrire le centre de gravité d'un Objet
-
-	public CentreGravite(double ax, double ay) {
-		super(ax, ay);
-	}
-
-}

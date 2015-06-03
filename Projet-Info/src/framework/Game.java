e14b6597e6c9c89260d3fd7f6a13c7a1cabdbbde
FALLOUH <FALLOUH@10.44.2.29>
2015-06-03 20:05:34 +0200

    2 stations si 2joueurs

--- Projet-Info/src/framework/Game.java
+++ Projet-Info/src/framework/Game.java
@@ -441,6 +441,15 @@ public class Game {
     				Objets.add(Station3);
     				Stations.add(Station3);
     			}
+    			else{
+    				Station3 = new Station((int)(CentreEcranX-(455*pW)),(int) (CentreEcranY+(256*pH)), Ecran,"DeathStar 3", creJoueurs[0]);
+    				Objets.add(Station3);
+    				Stations.add(Station3);
+    				Station4 = new Station((int)(CentreEcranX+(455*pW)), (int) (CentreEcranY+(256*pH)), Ecran,"DeathStar 4", creJoueurs[1]);
+    				Objets.add(Station4);
+    				Stations.add(Station4);
+    				
+    			}
     			if(creJoueurs[3]!=null){
     				Station4 = new Station((int)(CentreEcranX+(455*pW)), (int) (CentreEcranY+(256*pH)), Ecran,"DeathStar 4", creJoueurs[3]);
     				Objets.add(Station4);
